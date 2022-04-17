e75aac1592da3e711b72f187f39bfff28e87037c
cheran <cherankumar.548@gmail.com>
2022-04-17 21:51:28 +0530

    token creating

--- scripts/1_deploy_token.py
+++ scripts/1_deploy_token.py
@@ -2,6 +2,8 @@ from brownie import OurToken
 from scripts.helpful_scripts import get_account
 from web3 import Web3
 
+# if u want to sell this token online 
+# go to uniswap webpage
 intial_supply = Web3.toWei(1000, "ether")
 
 def main():
